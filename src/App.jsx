b33ae17576a258c23c4d63aee23a0eb7d25e65b1
Rushikesh Bobade <rushikesh9060@gmail.com>
2025-09-23 01:08:49 +0530

    Initial portfolio project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App= ()=> {
+  return (
+    <div className="text-3xl font-bold underline text-blue-600">
+       App is running...
+    </div>
+  )
+}
+
+export default App;
+
